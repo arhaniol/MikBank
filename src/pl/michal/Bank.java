5b2d7cc8b6612342d2ad653b41e43fae4eb0aa5a
arhaniol <arhaniol@op.pl>
2019-08-17 01:27:00 +0200

    mySQLsupport

--- src/pl/michal/Bank.java
+++ src/pl/michal/Bank.java
@@ -2,14 +2,17 @@ package pl.michal;
 
 import pl.michal.interfejs.BankAccount;
 import pl.michal.interfejs.BankTransfer;
+import pl.michal.interfejs.MySQLaccess;
 
 public class Bank implements BankTransfer {
     private int account;
     private final int FEE;
+    private MySQLaccess mySQLaccess;
 
     public Bank() {
         account = 0;
         FEE = 1;
+        mySQLaccess = new MySQLaccess();
     }
 
     @Override
@@ -22,7 +25,32 @@ public class Bank implements BankTransfer {
         account += FEE;
     }
 
+    public void transfer(BankAccount from, BankAccount to, double amount) {
+        if (amount <= 0) {
+            this.transfer(from, to, 0);
+        } else {
+            int fromNO = from.getAccountNO(),
+                    toNO = to.getAccountNO();
+
+            System.out.println(String.format("Transfer %.3f zl from %s account to %s account", amount, fromNO, toNO));
+
+            try {
+                from.withdraw(amount + FEE);
+
+                mySQLaccess.InsertBankTransaction(fromNO, toNO, amount, FEE);
+                mySQLaccess.FinishTransaction();
+            } catch (Exception e) {
+                throw new IllegalStateException("Transfer Error: " + e.toString());
+            }
+            account += FEE;
+        }
+    }
+
     public void printBankBalance() {
         System.out.println(String.format("Bank account: %d zl", account));
+    }
+
+    public void Finish() throws Exception {
+        mySQLaccess.CloseDB();
     }
 }
--- src/pl/michal/interfejs/BankAccount.java
+++ src/pl/michal/interfejs/BankAccount.java
@@ -1,16 +1,25 @@
 package pl.michal.interfejs;
 
+import java.util.Random;
+
 public class BankAccount implements Account {
-    private int accountBalance;
-    public String accountNO;
+    private double accountBalance;
+
+    public int getAccountNO() {
+        return accountNO;
+    }
+
+    private int accountNO;
     private static int orderNO = 1;
+    private static MySQLaccess mySQLaccess = new MySQLaccess();
+    private static Random rand = new Random();
 
     @Override
     public void deposit(int amount) {
         if (amount > 0)
             accountBalance += amount;
         else
-            throw new IllegalArgumentException("The amount must be greater than 0!!!");
+            throw new IllegalArgumentException("The amount must be greater than 0!!!\n");
     }
 
     @Override
@@ -19,14 +28,40 @@ public class BankAccount implements Account {
             if (amount <= accountBalance)
                 accountBalance -= amount;
             else
-                throw new IllegalStateException("Lack of account funds");
+                throw new IllegalStateException("Lack of account funds\n");
         } else
-            throw new IllegalArgumentException("The amount must be greater than 0!!!");
+            throw new IllegalArgumentException("The amount must be greater than 0!!!\n");
+    }
+
+    public void withdraw(double amount) throws Exception {
+        if (amount > 0) {
+            try {
+                double accB = mySQLaccess.GetAccountInfo(accountNO);
+                if ((accB - amount) < 0) {
+                    throw new IllegalStateException("Lack of account founds!\n");
+                }
+            } catch (Exception e) {
+                throw new Exception(e);
+            }
+        }
     }
 
     public BankAccount() {
-        accountBalance = 0;
         GenerateAccountNO();
+
+        accountBalance = rand.nextDouble() * 100;
+        String name = String.format("%c", rand.nextInt(24) + 65);
+        String lastName = String.format("%c", rand.nextInt(24) + 65);
+        String eMail = String.format("%s.%s@com.pl", name, lastName);
+        String city = String.format("%S%s%S%s%s%s", name, name, name, lastName, lastName, lastName);
+        try {
+            mySQLaccess.InsertClientData(accountNO, name, lastName, city, eMail);
+            mySQLaccess.InsertAccountData(accountNO, accountBalance, 10);
+            mySQLaccess.InsertBankTransaction(accountNO, accountNO, accountBalance, 0);
+            mySQLaccess.FinishTransaction();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 
     public BankAccount(int amount) {
@@ -35,11 +70,20 @@ public class BankAccount implements Account {
     }
 
     private void GenerateAccountNO() {
-        accountNO = String.format("0000 %d", orderNO++);
+//        accountNO = String.format("0000 %d", orderNO++);
+        accountNO = orderNO++;
     }
 
     @Override
     public void depositInfo() {
-        System.out.println(String.format("There is %d zł on account %s", accountBalance, accountNO));
+        try {
+            System.out.println(String.format("There is %.2f zł on account %s", mySQLaccess.GetAccountInfo(accountNO), accountNO));
+        } catch (Exception e) {
+            System.out.println("Problem to get Deposit Info\n");
+        }
+    }
+
+    public void Finish() throws Exception {
+        mySQLaccess.CloseDB();
     }
 }
